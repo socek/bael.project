7059dfe9de42c0b73da4ae72fa8f07d9dd57b0d2
Dominik "Socek" Dlugajczyk <msocek@gmail.com>
2014-08-12 21:40:51 +0200

    Fixed baelfire version.

--- setup.py
+++ setup.py
@@ -2,19 +2,19 @@
 from setuptools import setup, find_packages
 
 install_requires = [
-    'baelfire==0.2.3',
+    'baelfire==0.2.4',
     'virtualenv',
 ]
 
 if __name__ == '__main__':
     setup(name='bael.project',
           version='0.1.1',
+          description='Simple project generator.',
           license='Apache License 2.0',
           packages=find_packages('src'),
           package_dir={'': 'src'},
           namespace_packages=['bael'],
           install_requires=install_requires,
-          include_package_data=True,
           zip_safe=False,
           package_data={
               '': ['project/templates/*.tpl'],
