aa36dae6604b35d928d44e9f47266b318fe7892d
Dominik "Socek" Dlugajczyk <msocek@gmail.com>
2015-10-27 14:53:16 +0100

    Add command line tool.

---
+++ src/bael/project/cmd.py
@@ -0,0 +1,73 @@
+from argparse import ArgumentParser
+from baelfire.application.application import Application
+from baelfire.application.commands.graph.graph import Graph
+from logging import getLogger
+
+log = getLogger(__name__)
+
+
+class BaelApplication(Application):
+
+    tasks = {
+        'develop': 'bael.project.develop:Develop',
+        'create': 'bael.project.develop:Create',
+    }
+
+    def create_parser(self):
+        self.parser = ArgumentParser()
+        self._add_task_group()
+        self._add_logging_group()
+
+    def _add_task_group(self):
+        tasks = self.parser.add_argument_group(
+            'Tasks',
+            'Project related options',
+        )
+
+        tasks.add_argument(
+            '-c',
+            '--create',
+            dest='create',
+            help='Create full project path.',
+            action="store_true",
+        )
+        tasks.add_argument(
+            '-d',
+            '--develop',
+            dest='develop',
+            help='Download requiretments.',
+            action="store_true",
+        )
+        tasks.add_argument(
+            '-g',
+            '--graph',
+            dest='graph',
+            help='Draw task dependency graph.',
+            action="store_true",
+        )
+
+    def run_command_or_print_help(self, args):
+        if args.create or args.develop:
+            task = self._get_task(args)
+            try:
+                task.run()
+                report_path = task.save_report()
+            except:
+                log.error('Error in %(report)s' % task.paths)
+                raise
+            if args.graph:
+                Graph(report_path).render()
+        else:
+            self.parser.print_help()
+
+    def _get_task(self, args):
+        if args.create:
+            return self.import_task(self.tasks['create'])()
+        elif args.develop:
+            return self.import_task(self.tasks['develop'])()
+        else:
+            raise RuntimeError('No task selected!')
+
+
+def run():
+    BaelApplication().run()
